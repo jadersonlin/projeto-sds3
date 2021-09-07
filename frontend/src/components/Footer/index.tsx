73d1cf8223c352982ab9e118b668603d242ad653
Jaderson Linhares <jaderson_lin@hotmail.com>
2021-09-06 22:09:35 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-light">
+            <div className="container">
+                <p className="text-dark">App desenvolvido por <a href="https://github.com/jadersonlin" target="_blank" rel="noreferrer">Jaderson Linhares</a></p>
+                <p className="text-dark"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
